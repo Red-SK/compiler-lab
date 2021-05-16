2e5bc44655c9d863cfda00e5dea1513693d6eaa0
qizong007 <978451432@qq.com>
2021-05-16 15:43:50 +0800

    类型声明

---
+++ tokenizer/tokenizer.h
@@ -0,0 +1,53 @@
+#ifndef _TOKENIZER_H_
+#define _TOKENIZER_H_
+
+#include "common.h"
+
+enum TokenType {
+    UNKNOWN, // 未知类型
+    INT,
+    REAL, // 实数
+    BOOL,
+    TRUE,
+    FALSE,
+    IF,
+    ELIF, // else-if
+    ELSE,
+    FOR,
+    RETURN,
+    BREAK,
+    ID, //标识符
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+    ASSIGN,
+    EQUAL,
+    NOT_EQUAL,
+    AND,
+    OR,
+    NOT,
+    GREATER,
+    GREATER_EQAUL,
+    LESS,
+    LESS_EQUAL,
+    COMMA, // ,
+    SEMICOLON, // ;
+    LEFT_BLOCK,
+    RIGHT_BLOCK,
+    SQ_LEFT_BRACKET, // [
+    SQ_RIGHT_BRACKET, // ]
+    CIR_LEFT_BRACKET, // (
+    CIR_RIGHT_BRACKET, // )
+    ERROR
+}
+
+struct Token {
+    const char* start; // 字符指针，指向token内容的起始位置
+    string lex; // 词素(内容本身)
+    TokenType type; // Token类型
+    uint curLine; // 当前行
+    uint length;
+};
+
+#endif
